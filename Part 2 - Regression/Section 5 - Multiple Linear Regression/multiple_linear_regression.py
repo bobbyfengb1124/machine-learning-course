9cd80d6f4f63e5ca954afc1e9e82a619d419039d
bofeng@u.nus.edu <bobbyfeng@cse.com.sg>
2018-03-16 11:12:20 +0800

    added multiple regressor and test results

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -43,3 +43,12 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# FittingMultipleLinearRegressionto the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+
+# Predicting the test set results
+
